f612a85af9ba36dd4f098443e04ae3c98c9436df
phamhung107 <pvh1072002@gmail.com>
2024-01-09 21:57:00 +0700

    connection pool

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,17 @@
+const connection = require ('../config/database')
+
+
 const getHomepage = (req, res) =>{
-    res.render('sample.ejs')
+
+    
+    connection.query(
+        'select * from Users u',
+        function(err, results, fields){
+            users = results;
+            console.log(results);
+            res.send(JSON.stringify(users))
+        }
+    );
 }
 const getABC = (req, res) =>{
     res.send('check ABC')
